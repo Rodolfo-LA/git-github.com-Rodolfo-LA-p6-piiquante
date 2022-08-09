dfd3c7816fc0f4223bba90837fe04bcd8184516c
Rodolfo-LA <lopez.rodolfo@neuf.fr>
2022-08-09 18:41:03 +0200

    Autorisation du cross origin.

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 // Utilisation des middlewares gestion json & sécurité express
 
 app.use(express.json());
-app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 // Utilisation des middlewares liés aux différentes routes
 
